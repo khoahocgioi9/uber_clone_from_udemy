b621fa5dac06dad9c9888f3c05894175a7c530cc
minh khoa <khoahocgioi9@gmail.com>
2023-08-31 13:54:26 +0700

    first commit

---
+++ users_app/lib/global/global.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:users_app/models/direction_details_info.dart';
+import 'package:users_app/models/user_model.dart';
+
+
+
+final FirebaseAuth fAuth = FirebaseAuth.instance;
+User? currentFirebaseUser;
+UserModel? userModelCurrentInfo;
+List dList = []; //online-active drivers Information List
+DirectionDetailsInfo? tripDirectionDetailsInfo;
+String? chosenDriverId="";
+String cloudMessagingServerToken = "key=AAAAqo4oJOg:APA91bHuVtVtkuF6JeHkiwt7J5LY6m2yRPDIwX1fyD_RtNcUcyRkWbnzEr1D0Oo06pG-ekgS29aJ-wVUefdV1jqn5itSXm-OM29WznvZwZCEGZpXTZRaZxXVMKQMx7VXiYBANHoaoTjQ";
+String userDropOffAddress = "";
+String driverCarDetails="";
+String driverName="";
+String driverPhone="";
+double countRatingStars=0.0;
+String titleStarsRating="";
